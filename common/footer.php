1729c846d6538bf595566ea60f103a268252a002
csbailey5t <csb5t@virginia.edu>
2015-01-27 16:35:54 -0500

    initial commit

---
+++ common/footer.php
@@ -0,0 +1,15 @@
+    </div>
+
+    <footer role="contentinfo">
+
+        <nav role="navigation">
+            <?php echo public_nav_main(); ?>
+        </nav>
+
+        <p class="credits"><em>Catalogue of Ships</em> is designed and maintained by the <a href="http://scholarslab.org">Scholars’ Lab</a> at the <a href="http://library.virginia.edu">University of Virginia Library</a>.</p>
+
+        <?php fire_plugin_hook('public_theme_footer'); ?>
+
+    </footer>
+</body>
+</html>
